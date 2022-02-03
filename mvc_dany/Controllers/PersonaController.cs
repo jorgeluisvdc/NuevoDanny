e1f713bce44753c5cb8bc43521b80cfc60e795cb
jorgeluisvdc <jorge.luisvdc@gmail.com>
2022-02-03 12:13:50 -0500

    esta debe ir

--- mvc_dany/Controllers/PersonaController.cs
+++ mvc_dany/Controllers/PersonaController.cs
@@ -35,7 +35,7 @@ namespace mvc_dany.Controllers
             */
 
             // ejercicio 3 consulta con 2 tablas ***************************
-            // eres una perrita coketa!!!
+            
             var oLista = (from p in oPer
                           join c in oCur on p.codigo equals c.codigo
                           select new PersonaConsulta
